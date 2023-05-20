a916e7209b1ef429950d9b523491d8c11d0f7590
muril <muril@DESKTOP-TC38L2I>
2023-05-20 17:08:35 -0300

    commit

--- src/tiposVariaveis/TiposDeVariaveis.java
+++ src/tiposVariaveis/TiposDeVariaveis.java
@@ -11,10 +11,13 @@ public class TiposDeVariaveis {
 		float pi = 3.14F;
 		long cep = 38425354L;
 		int cast = 2147483647;
-		
+		String nome1 = "murilo";
+		String nome2 = "alexandre";
 		mes = (short) cast;
+		boolean igual;
+		igual = nome1.equals(nome2);
 		
-		System.out.println(mes); // vai dar -1 pois deu overflow no mes que é short
+		System.out.println(igual); // vai dar -1 pois deu overflow no mes que é short
 		
 		// teste de pull do git para o eclipse
 
