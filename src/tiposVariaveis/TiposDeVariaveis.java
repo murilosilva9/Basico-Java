10b338626de5164cb354e55dc6cd8c2da62b3149
muril <muril@DESKTOP-TC38L2I>
2023-05-20 10:20:41 -0300

    commit 2 tipagem

--- src/tiposVariaveis/TiposDeVariaveis.java
+++ src/tiposVariaveis/TiposDeVariaveis.java
@@ -4,6 +4,12 @@ public class TiposDeVariaveis {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		double salarioMinimo = 6000.99;
+		int ano = 2023;
+		short mes = 01;
+		float pi = 3.14F;
+		long cep = 38425354L;
 
 	}
 
